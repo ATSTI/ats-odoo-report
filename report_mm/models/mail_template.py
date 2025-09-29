9353b0815ca043527177545fb075fbd296564a26
OtavioAndretta <otavio12257@gmail.com>
2025-09-29 17:39:40 -0300

    default_reports

--- report_mm/models/mail_template.py
+++ report_mm/models/mail_template.py
@@ -12,7 +12,7 @@ class MailTemplate(models.Model):
 
     def generate_email(self, res_ids, fields):
         """Versão adaptada para suportar vários relatórios (Many2many) 
-        e exibir nomes dos relatórios no wizard."""
+        e anexar automaticamente relatórios específicos."""
         self.ensure_one()
         multi_mode = True
         if isinstance(res_ids, int):
@@ -21,7 +21,7 @@ class MailTemplate(models.Model):
 
         results = dict()
         for lang, (template, template_res_ids) in self._classify_per_lang(res_ids).items():
-           
+
             for field in fields:
                 generated_field_values = template._render_field(
                     field, template_res_ids,
@@ -30,7 +30,6 @@ class MailTemplate(models.Model):
                 for res_id, field_value in generated_field_values.items():
                     results.setdefault(res_id, dict())[field] = field_value
 
- 
             if any(field in fields for field in ['email_to', 'partner_to', 'email_cc']):
                 results = template.generate_recipients(results, template_res_ids)
 
@@ -43,7 +42,6 @@ class MailTemplate(models.Model):
                     parsed_datetime = self.env['mail.mail']._parse_scheduled_datetime(scheduled_date)
                     values['scheduled_date'] = parsed_datetime.replace(tzinfo=None) if parsed_datetime else False
 
-              
                 values.update(
                     mail_server_id=template.mail_server_id.id or False,
                     auto_delete=template.auto_delete,
@@ -52,31 +50,39 @@ class MailTemplate(models.Model):
                     attachment_ids=[attach.id for attach in template.attachment_ids],
                 )
 
-     
-            if template.report_template:
-                for res_id in template_res_ids:
-                    attachments = results[res_id].setdefault('attachments', [])
-                    report_display_names = results[res_id].setdefault('report_names', [])
+            
+            default_reports = self.env['ir.actions.report'].search([
+                ('report_name', 'in', [
+                    'report_mm.report_orcamento_mmportas'
+                    'report_mm.report_contrato_mmportas',
+                    'report_mm.report_ambos_mmportas',
+                    'report_mm.report_contrato_stand_mmportas',
+                    'report_mm.report_contrato_orcamento_stand_mmportas',
+                ])
+            ])
 
-                    for report in template.report_template:
-                        
-                        report_name = report.name
-                        report_display_names.append(report_name)
+            for res_id in template_res_ids:
+                attachments = results[res_id].setdefault('attachments', [])
+                report_display_names = results[res_id].setdefault('report_names', [])
 
-                        if report.report_type in ['qweb-html', 'qweb-pdf']:
-                            result, report_format = self.env['ir.actions.report']._render_qweb_pdf(report, [res_id])
-                        else:
-                            res = self.env['ir.actions.report']._render(report, [res_id])
-                            if not res:
-                                raise UserError(_('Unsupported report type %s found.', report.report_type))
-                            result, report_format = res
+                for report in default_reports:
+                    report_name = report.name
+                    report_display_names.append(report_name)
 
-                        result = base64.b64encode(result)
+                    if report.report_type in ['qweb-html', 'qweb-pdf']:
+                        result, report_format = self.env['ir.actions.report']._render_qweb_pdf(report, [res_id])
+                    else:
+                        res = self.env['ir.actions.report']._render(report, [res_id])
+                        if not res:
+                            raise UserError(_('Unsupported report type %s found.', report.report_type))
+                        result, report_format = res
 
-                        ext = "." + report_format
-                        if not report_name.endswith(ext):
-                            report_name += ext
+                    result = base64.b64encode(result)
 
-                        attachments.append((report_name, result))
+                    ext = "." + report_format
+                    if not report_name.endswith(ext):
+                        report_name += ext
+
+                    attachments.append((report_name, result))
 
         return multi_mode and results or results[res_ids[0]]
