6a5a26801a93cae67d79922db85acb81bc1e0233
OtavioAndretta <otavio12257@gmail.com>
2026-08-10 10:28:01 -0300

    Report faturas para o CALDANA

---
+++ report_caldana/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2025 - ATSTi
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Relatórios personalizados para o Caldana',
+    'version': '16.0',
+    'category': 'Others',
+    'license': 'AGPL-3',
+    'sequence': 2,
+    'summary': 'ATSTi Soluções',
+    'description': """
+            Relatorio personalizado para o Caldana
+    """,
+    'author': 'ATSTi,Odoo Community Association (OCA)',
+    'maintainer': 'OtavioAndretta <otavio12257@gmail.com>',
+    'website': '',
+    'depends': [
+        'base','sale','account'
+    ],
+    'data': [
+        'report/report_faturas_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
