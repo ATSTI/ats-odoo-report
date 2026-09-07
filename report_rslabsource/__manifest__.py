080be56dafe773e2dfb5d0dc27df6e091b618794
crsilveira <crsilveira@gmail.com>
2026-09-07 13:32:41 -0300

    upgrade odoo 16

---
+++ report_rslabsource/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-Today OpenERP S.A. (<http://www.openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Relatorio De orçamento personalizado/ RsLabSource',
+    "version": "16.0.0.1.0",
+    'category': 'Others',
+    "license": "AGPL-3",
+    'sequence': 2,
+    'summary': 'relatorios personalizados',
+    'description': """ Relatorio customizado para a rslabsource de cotação/pedido
+    """,
+    'category': 'Contacts/Custom',
+    'author': 'ATSTi',
+    'website': 'http://www.atsti.com.br',
+    'contributors': [
+        'Otavio Andretta <otavio12257@gmail.com>'
+    ],
+    'depends': ['account', 'l10n_br_sale', 'l10n_br_purchase'],
+    'data': [
+        'report/report_paper_format.xml',
+        'views/sale_order.xml',
+        'views/purchase_order.xml',
+        'report/report_rslabsource_orcamento_document.xml',
+        'report/report_rslabsource_pedido_venda_document.xml',
+        'report/report_rslabsource_pedido_compra_document.xml',
+        'report/report_rslabsource_orcamento_document.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
